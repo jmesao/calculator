de4e1b3e826d9d8e64c6c3ab0dfdf328bfccf635
Javier Mesa <31845091+jmesao@users.noreply.github.com>
2023-03-18 21:31:33 +0100

    Feat/vue3 typescript pinia setup (#1)
    * feat/Add gitignore
    
    * feat/Add package.json, tsconfig and vite
    
    * feat/Add eslint and prettier
    
    * feat/Add favicon and index.html
    
    * feat/Add package-lock.json
    
    * feat/Add basic css for the app
    
    * feat/Add logo
    
    * feat/Create HeaderInfo component and app
    
    * feat/Mount app and use HeaderInfo component
    
    * feat/Update README file

---
+++ src/components/HeaderInfo.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  subtitle: string;
+}>();
+</script>
+
+<template>
+  <div class="header-info">
+    <img alt="Vue logo" class="logo" src="../assets/logo.svg" width="125" height="125" />
+    <h1 class="header-info__title">{{ title }}</h1>
+    <h3 class="header-info__subtitle">{{ subtitle }}</h3>
+  </div>
+</template>
+
+<style lang="scss">
+.header-info {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #fff;
+
+  &__title {
+    font-weight: 500;
+    font-size: 2.6rem;
+  }
+
+  &__subtitle {
+    font-size: 1.2rem;
+  }
+}
+</style>
