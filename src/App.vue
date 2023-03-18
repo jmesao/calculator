80be1a280f332a827f2c34d87328841f40f5ff2c
Javier Mesa <francisco-javier.mesa-olmo@ankorstore.com>
2023-03-18 21:17:05 +0100

    feat/Create HeaderInfo component and app

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HeaderInfo from './components/HeaderInfo.vue';
+</script>
+
+<template>
+  <main>
+    <HeaderInfo title="Calculator" subtitle="Vue 3 + Typescript + Pinia" />
+  </main>
+</template>
